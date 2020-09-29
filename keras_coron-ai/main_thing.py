74e552dbdcb24390370dbd4255134e8526f8379c
sahal-mulki <sahal.mulki@gmail.com>
2020-09-29 11:23:06 +0400

    Changed the script

--- keras_coron-ai/main_thing.py
+++ keras_coron-ai/main_thing.py
@@ -12,30 +12,9 @@ import PIL
 import tensorflow as tf
 import numpy as np
 
-model_selection = os.getenv('model_select')
+class_names = ["Normal", "Pnuemonia"]
 
-if model_selection == None:
-    input0 = input("Would you like to use .(H)5 or (S)avedModel")
-    if input0 == "H":
-        os.environ['model_select'] = 'H'
-        global model
-        model = keras.models.load_model('xrayble_2.h5')
-    elif input0 == "S":
-        os.environ['model_select'] = 'S'
-        model = keras.models.load_model('xrayble_2')
-elif model_selection == "H":
-    model = keras.models.load_model('xrayble_2.h5')
-elif model_selection == "S":
-    model = keras.models.load_model('xrayble_2')
-
-
-if input0 == "H":
-    model = keras.models.load_model('xrayble_2.h5')
-elif input0 == "S":
-    model = keras.models.load_model('xrayble_2')
-
-img_height = 180
-img_width = 180
+model = keras.models.load_model('xrayble')
 
 NORM_FONT = ("Verdana", 11)
 
@@ -46,22 +25,23 @@ def pick_file():
 
     global filename
     filename = root2.filename
-  
+
 def popupmsg(msg):
     popup = Tk()
     popup.wm_title("Result")
-    
+
     label = ttk.Label(popup, text=msg, font=NORM_FONT)
 
     label.pack(anchor='center')
     B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
     B1.pack()
     popup.mainloop()
+    os._exit(0)
 
 def popupmsg2(msg):
     popup = Tk()
     popup.wm_title("Analyze?")
-    
+
     label = ttk.Label(popup, text=msg, font=NORM_FONT)
 
     label.pack(anchor='center')
@@ -75,7 +55,7 @@ messagebox.showinfo("Analyze?", "Analyze? It will take about 30 seconds")
 print("")
 
 img = keras.preprocessing.image.load_img(
-    filename, target_size=(img_height, img_width)
+    filename, target_size=(180, 180)
 )
 
 img_array = keras.preprocessing.image.img_to_array(img)
