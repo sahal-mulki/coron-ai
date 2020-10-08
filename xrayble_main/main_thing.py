c67b2a7c16326581a7b2800ce2096cf327f7ad35
Muhammad Sahal Mulki <56467460+sahal-mulki@users.noreply.github.com>
2020-10-08 08:13:30 +0400

    Update main_thing.py

--- xrayble_main/main_thing.py
+++ xrayble_main/main_thing.py
@@ -16,47 +16,14 @@ class_names = ["Normal", "Pnuemonia"]
 
 model = keras.models.load_model('xrayble')
 
-NORM_FONT = ("Verdana", 11)
-
-def pick_file():
-    root2 = Tk()
-    root2.withdraw()
-    root2.filename = askopenfilename()
-
-    global filename
-    filename = root2.filename
-
-def popupmsg(msg):
-    popup = Tk()
-    popup.wm_title("Result")
-
-    label = ttk.Label(popup, text=msg, font=NORM_FONT)
-
-    label.pack(anchor='center')
-    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
-    B1.pack()
-    popup.mainloop()
-    os._exit(0)
-
-def popupmsg2(msg):
-    popup = Tk()
-    popup.wm_title("Analyze?")
-
-    label = ttk.Label(popup, text=msg, font=NORM_FONT)
-
-    label.pack(anchor='center')
-    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
-    B1.pack()
-    popup.mainloop()
-
-pick_file()
-
-messagebox.showinfo("Analyze?", "Analyze?")
-print("")
-
-img = keras.preprocessing.image.load_img(
-    filename, target_size=(180, 180)
-)
+img_path = input("Enter the path of your image")
+try:
+    img = keras.preprocessing.image.load_img(
+        img_path, target_size=(180, 180)
+    )
+except:
+    print('Path not found')
+    exit(0)
 
 img_array = keras.preprocessing.image.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0) # Create a batch
@@ -67,4 +34,4 @@ score = tf.nn.softmax(predictions[0])
 wow = [class_names[np.argmax(score)], 100 * np.max(score)]
 
 yay = "This is " + wow[0] + ": " + str(wow[1])
-popupmsg(yay)
+print(yay)
