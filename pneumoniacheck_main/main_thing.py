0e49d393d66875a0c339bd2e59f5cd77567db801
Muhammad Sahal Mulki <56467460+sahal-mulki@users.noreply.github.com>
2021-02-04 15:07:24 +0400

    thing

---
+++ pneumoniacheck_main/main_thing.py
@@ -0,0 +1,71 @@
+from PIL import ImageOps
+from tkinter import filedialog, ttk, Tk
+from PIL import ImageTk, Image
+from tkinter.filedialog import askopenfilename
+from tkinter import messagebox
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow_addons.optimizers import RectifiedAdam
+
+import os, PIL, tensorflow.keras
+import PIL
+import tensorflow as tf
+import numpy as np
+
+class_names = ["Normal", "Pnuemonia"]
+
+model = keras.models.load_model('pneumoniacheck')
+
+NORM_FONT = ("Verdana", 11)
+
+def pick_file():
+    root2 = Tk()
+    root2.withdraw()
+    root2.filename = askopenfilename()
+
+    global filename
+    filename = root2.filename
+
+def popupmsg(msg):
+    popup = Tk()
+    popup.wm_title("Result")
+
+    label = ttk.Label(popup, text=msg, font=NORM_FONT)
+
+    label.pack(anchor='center')
+    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
+    os._exit(0)
+
+def popupmsg2(msg):
+    popup = Tk()
+    popup.wm_title("Analyze?")
+
+    label = ttk.Label(popup, text=msg, font=NORM_FONT)
+
+    label.pack(anchor='center')
+    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
+pick_file()
+
+messagebox.showinfo("Analyze?", "Analyze?")
+print("")
+
+img = keras.preprocessing.image.load_img(
+    filename, target_size=(180, 180)
+)
+
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) 
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+wow = [class_names[np.argmax(score)], 100 * np.max(score)]
+
+yay = "This is " + wow[0] + ": " + str(wow[1])
+popupmsg(yay)
